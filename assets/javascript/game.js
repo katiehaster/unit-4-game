cb06c70298db5aeef3fda402fa78a564ee1c5b55
kth <katiehaster@gmail.com>
2019-03-04 18:35:26 -0800

    First Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+
+var randomNumber = 0;
+var Leo;
+var Princess;
+var General;
+var Teddy;
+var numTally = 0;
+var losses = 0;
+var wins = 0;
+var score = 0;
+
+
+(document).ready(function() {
+    Math.floor((Math.random() * 120) + 19);
