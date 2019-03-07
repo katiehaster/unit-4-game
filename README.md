338866678086ef048f327455d640ff5d12eb86a8
kth <katiehaster@gmail.com>
2019-03-06 18:17:56 -0800

    Final Work

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # unit-4-game
+
+To get in the hang of README's I'm going to do this:
+
+What the project does:
+
+This game allows users to interact with what they see on the page to play a simple game, kind of like an interactive game of "Memory."
+
+Why the project is useful:
+
+It was more useful to me as a learning tool than it would be to anone who plays it.
+
+How users can get started with the project:
+
+Begin to play by clicking on a portrait!
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,38 @@
 
-var randomNum = 0;
-var Leo = 0;
-var Princess = 0;
-var Gray = 0;
-var Teddy = 0;
-var numTally = 0;
+var randomNum;
+var Leo;
+var Princess;
+var Gray;
+var Teddy;
+var numTally;
 var losses = 0;
 var wins = 0;
 
 
 
 $(document).ready(function() {
+
+start();
     
     // Generates and displays random number
-    randomNum = Math.floor((Math.random() * 120) + 19);
-    console.log("RanNum: " + randomNum);
-    $("#randomNumber").text("Random Number: " + randomNum);
+   function start() { 
+       numTally = 0;
+       console.log("start function");
+       randomNum = Math.floor((Math.random() * 120) + 19);
+        console.log("RanNum: " + randomNum);
+        $("#randomNumber").text("Random Number: " + randomNum);
+        $("#totalScore").text("Total Score: " + numTally);
+    }
 
     Leo = Math.floor((Math.random() * 12) + 1);
-    console.log("Leo: " + Leo);
-
+    console.log("Leo: " + Leo); 
+    
     Princess = Math.floor((Math.random() * 12) + 1);
     console.log("Princess: " + Princess);
+        // Trying to have a diff # for each
+        if (Princess === Leo) {
+        Princess = Math.floor((Math.random() * 12) + 1);
+        }
 
     Gray = Math.floor((Math.random() * 12) + 1);
     console.log("Gray: " + Gray);
@@ -29,27 +40,58 @@ $(document).ready(function() {
     Teddy = Math.floor((Math.random() * 12) + 1);
     console.log("Teddy: " + Teddy);
 
+    // Adds each click of portrait to total score
 
     $("#Ruffles").on("click", function () {      
         numTally = numTally + Leo;
         $("#totalScore").text("Total Score: " + numTally);
+        checkWinLoss();
     });
 
     $("#Petals").on("click", function () {      
         numTally = numTally + Princess;
         $("#totalScore").text("Total Score: " + numTally);
+        checkWinLoss();
     });
 
     $("#General").on("click", function () {      
         numTally = numTally + Gray;
         $("#totalScore").text("Total Score: " + numTally);
+        checkWinLoss();
     });
 
     $("#Glasses").on("click", function () {      
         numTally = numTally + Teddy;
+        console.log(numTally);
         $("#totalScore").text("Total Score: " + numTally);
-    });
+        checkWinLoss();
+    } 
+    );
 
+    // When random number = score, "you win!"
+
+ 
+    function checkWinLoss() {
+    if (randomNum === numTally) {
+        $("#wins").text(wins + 1);
+        alert("Paws-itively Great!");
+        start();
+    }
+    
+    else if (randomNum < numTally) {
+        $("#losses").text(losses + 1);
+        alert('"Pet"-ter Luck Next Time');
+        start();
+    }
+    
+    }
+
+    
+
+       
+    
+
+    
 
 
 
